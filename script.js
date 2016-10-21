c03a89aa42aac2da94feb503cc55501c9f16f3bd
arseniypetrikor <arseniy.beresnev@gmail.com>
2016-10-21 19:14:43 +0500

    Added content loading
    - Now we can load 'counter' variable from server side

---
+++ script.js
@@ -0,0 +1,11 @@
+function loadData() {
+  var xhr = XMLHttpRequest();
+  xhr.addEventListener('load', function () {
+    var elem = document.getElementById('counter');
+    elem.innerHTML = xhr.responseText;
+  });
+  xhr.open('GET', 'http://localhost/get');
+  xhr.send();
+}
+
+window.addEventListener('load', loadData);
